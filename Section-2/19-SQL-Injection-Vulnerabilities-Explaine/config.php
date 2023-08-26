ddf72d6642048cf043375b5503fe09098f29e7e6
Mahmoud Matouk <110528830+Mahmoud-Matouk@users.noreply.github.com>
2023-08-26 19:49:50 +0300

    first commit

---
+++ Section-2/19-SQL-Injection-Vulnerabilities-Explaine/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'dataBase' => [
+        'host'     => 'localhost',
+        'port'     => '3306',
+        'username' => 'root',
+        'password' => '',
+        'dbname'   => 'muc',
+        'charset'  => 'utf8mb4'  
+    ],
+];
