f14b29710e83b98354a998e18c3fa4e163dff54a
Mahmoud Matouk <110528830+Mahmoud-Matouk@users.noreply.github.com>
2023-08-31 13:50:12 +0300

    first commit

--- Section-3/25-Intro-to-Forms-and-Request-Methods/controllers/views/contact.view.php
+++ Section-3/25-Intro-to-Forms-and-Request-Methods/controllers/views/contact.view.php
@@ -3,8 +3,7 @@
 <?php require "partials/header.php"; ?>
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>Welcome To Home Page</p>
+        <p>Welcome To Contact Us Page</p>
     </div>
 </main>
 <?php require "partials/footer.php"; ?>
-<?php require "/system/xampp/htdocs/working/Section-3/24-Programming-is-Rewriting/router.php"; ?>
